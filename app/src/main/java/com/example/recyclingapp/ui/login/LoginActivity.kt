78354782a05809147c85de9876f566758c7dd609
Mustafa Ünlü <mustafa.unlum18@gmail.com>
2024-02-04 16:57:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/recyclingapp/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclingapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclingapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
