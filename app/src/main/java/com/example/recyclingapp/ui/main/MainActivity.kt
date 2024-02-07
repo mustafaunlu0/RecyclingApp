41d495e80c913963818008d99b354cfba6fed479
Mustafa Ünlü <mustafa.unlum18@gmail.com>
2024-02-07 12:36:00 +0300

    Added bottom nav menu and edited xml files.

--- app/src/main/java/com/example/recyclingapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/recyclingapp/ui/main/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.recyclingapp.ui.main
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recyclingapp.R
+import com.example.recyclingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding :ActivityMainBinding? = null
+    @SuppressLint("UseCompatLoadingForColorStateLists")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.bottomNavigationView?.background = null
+        binding?.bottomNavigationView?.menu?.getItem(2)?.isEnabled = false
+        binding?.fab?.backgroundTintList = getResources().getColorStateList(R.color.green);
+        binding?.fab?.imageTintList = getResources().getColorStateList(R.color.white);
+
     }
 }
